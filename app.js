96a41bf4552097c321eb946d85839695de0ad607
Vinicius Moll <31710386+viniciusmoll7th@users.noreply.github.com>
2019-05-22 11:56:27 -0300

    Ajuste de descrição.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('Hello Geeks!');
 });
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('Servidor na porta 3000!');
 });
